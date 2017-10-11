169a4267253719206181a52e33a316825558f54f
Jesse Lingeman <jmlingeman@gmail.com>
2017-10-11 10:39:10 -0400

    Compile using JFX packager for now instead of appbundler

--- compile_osx.sh
+++ compile_osx.sh
@@ -12,4 +12,7 @@
 #
 curl http://www.datavyu.org/docs/user-guide.pdf > packaged_docs/user-guide.pdf
 # The arguments 'package' and 'appbundle:bundle' are there to create an application bundle
-mvn clean package appbundle:bundle -Dmaven.test.skip=true jfx:native
+# mvn clean package appbundle:bundle -Dmaven.test.skip=true jfx:native
+
+# This line builds using the JFX packager
+mvn clean -Dmaven.test.skip=true jfx:native
