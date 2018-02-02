e74f3511eb998f09167bae34e29c2c4029fc5d87
Reda Nezzar <nezzar.reda@gmail.com>
2018-02-02 14:33:39 -0500

    bugFix-DVD-66 remove move left and right from the menu bar

--- src/main/java/org/datavyu/views/DatavyuView.java
+++ src/main/java/org/datavyu/views/DatavyuView.java
@@ -135,8 +135,9 @@ public final class DatavyuView extends FrameView implements FileDropEventListene
     private javax.swing.JMenuItem newCellLeftMenuItem;
     private javax.swing.JMenuItem newCellMenuItem;
     private javax.swing.JMenuItem newCellRightMenuItem;
-    private javax.swing.JMenuItem selectColumnLeftMenuItem;
-    private javax.swing.JMenuItem selectColumnRightMenuItem;
+    //TODO: romove the selectColumnLeft and Right Menu item
+//    private javax.swing.JMenuItem selectColumnLeftMenuItem;
+//    private javax.swing.JMenuItem selectColumnRightMenuItem;
     private javax.swing.JMenuItem newMenuItem;
     private javax.swing.JMenuItem closeTabMenuItem;
     private javax.swing.JMenuItem newVariableMenuItem;
@@ -224,9 +225,9 @@ public final class DatavyuView extends FrameView implements FileDropEventListene
         // BugzID:521 + 468 - Define accelerator keys based on Operating system.
         int keyMask = Toolkit.getDefaultToolkit().getMenuShortcutKeyMask();
 
-        // Actually handled in SpreadsheetPanel
-        selectColumnLeftMenuItem.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_LEFT, keyMask));
-        selectColumnRightMenuItem.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_RIGHT, keyMask));
+        //TODO: Actually handled in SpreadsheetPanel, no need to have to shortcut
+//        selectColumnLeftMenuItem.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_LEFT, keyMask));
+//        selectColumnRightMenuItem.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_RIGHT, keyMask));
 
         weakTemporalAlignmentMenuItem.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_T, keyMask));
 
@@ -1908,8 +1909,9 @@ public final class DatavyuView extends FrameView implements FileDropEventListene
         weakTemporalAlignmentMenuItem = new javax.swing.JCheckBoxMenuItem();
         zoomMenu = new javax.swing.JMenu();
         zoomInMenuItem = new javax.swing.JMenuItem();
-        selectColumnLeftMenuItem = new javax.swing.JMenuItem();
-        selectColumnRightMenuItem = new javax.swing.JMenuItem();
+        //TODO: romve from the menu bar
+//        selectColumnLeftMenuItem = new javax.swing.JMenuItem();
+//        selectColumnRightMenuItem = new javax.swing.JMenuItem();
         zoomOutMenuItem = new javax.swing.JMenuItem();
         jSeparator5 = new javax.swing.JSeparator();
         resetZoomMenuItem = new javax.swing.JMenuItem();
@@ -2155,13 +2157,14 @@ public final class DatavyuView extends FrameView implements FileDropEventListene
         });
         spreadsheetMenu.add(weakTemporalAlignmentMenuItem);
 
-        selectColumnLeftMenuItem.setName("Select Column (Left)");
-        selectColumnLeftMenuItem.setAction(actionMap.get("selectColumnLeft"));
-        spreadsheetMenu.add(selectColumnLeftMenuItem);
-
-        selectColumnRightMenuItem.setName("Select Column (Right)");
-        selectColumnRightMenuItem.setAction(actionMap.get("selectColumnRight"));
-        spreadsheetMenu.add(selectColumnRightMenuItem);
+        //TODO: Remove from the menu bar
+//        selectColumnLeftMenuItem.setName("Select Column (Left)");
+//        selectColumnLeftMenuItem.setAction(actionMap.get("selectColumnLeft"));
+//        spreadsheetMenu.add(selectColumnLeftMenuItem);
+//
+//        selectColumnRightMenuItem.setName("Select Column (Right)");
+//        selectColumnRightMenuItem.setAction(actionMap.get("selectColumnRight"));
+//        spreadsheetMenu.add(selectColumnRightMenuItem);
 
         zoomMenu.setName("zoomMenu");
 
